ffdca622c044db5ca3d3596a80fd3b3fc3406700
nizuoiuh <nizuoiuh@users.noreply.github.com>
2023-11-19 01:50:01 +0100

    added some coordinates, fought a lost battle with removing_oval_edges

--- aoepro.py
+++ aoepro.py
@@ -11,6 +11,12 @@ import pytesseract
 def capture_screen():
     screen = ImageGrab.grab(bbox=(0, 0, screen_resolution[0], screen_resolution[1]))
     return cv2.cvtColor(np.array(screen), cv2.COLOR_RGB2BGR)
+    
+
+def remove_oval_edges(adaptive_threshold_output):
+    # Crop the image to remove 10 pixels from all sides
+    cropped_image = adaptive_threshold_output[10:-10, 10:-10]
+    return cropped_image   
 
 # Function to extract text from an image
 def extract_text(image):
@@ -29,7 +35,8 @@ def extract_text(image):
     gray = cv2.cvtColor(white_text, cv2.COLOR_BGR2GRAY)
 
     # Apply adaptive thresholding
-    threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 31, 5)
+    threshold = remove_oval_edges(cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 31, 5))
+
 
     text = pytesseract.image_to_string(threshold)
     return text.strip(), threshold  # Return both the text and the thresholded image
@@ -40,10 +47,17 @@ def draw_rectangles(image, regions):
     for region in regions:
         cv2.rectangle(image, (region[0], region[1]), (region[2], region[3]), (0, 255, 0), 2)
 
+# Get the screen width and height
+screen_width, screen_height = pyautogui.size()
+print(f"Screen Resolution: {screen_width}x{screen_height}")
+
 # Set your screen resolution
-screen_resolution = (1920, 1080)  # Change this to your screen resolution
+screen_resolution = (screen_width,screen_height)  # Change this to your screen resolution
 
 # Define coordinates for the villager queue and game time
+population_coordinates = (590,20,675,60)
+villager_number_coordinates = (575,40,600,65)
+villager_idle_coordinates = (710,20,750,60)
 villager_queue_coordinates = (0, 80, 300, 120)  # Adjust these coordinates based on your screen
 game_time_coordinates = (1130, 0, 1220, 40)  # Adjust these coordinates based on your screen
 
@@ -53,17 +67,29 @@ while True:
     game_screen = capture_screen()
 
     # Draw rectangles around regions of interest
-    draw_rectangles(game_screen, [villager_queue_coordinates, game_time_coordinates])
+    draw_rectangles(game_screen, [villager_queue_coordinates, game_time_coordinates,villager_number_coordinates,villager_idle_coordinates,population_coordinates])
 
     # Extract text from the regions
     villager_queue_text, villager_queue_threshold = extract_text(game_screen[villager_queue_coordinates[1]:villager_queue_coordinates[3],
                                                                             villager_queue_coordinates[0]:villager_queue_coordinates[2]])
+    villager_number_text, villager_number_threshold = extract_text(game_screen[villager_number_coordinates[1]:villager_number_coordinates[3],
+                                                                            villager_number_coordinates[0]:villager_number_coordinates[2]])
+    villager_idle_text, villager_idle_threshold = extract_text(game_screen[villager_idle_coordinates[1]:villager_idle_coordinates[3],
+                                                                            villager_idle_coordinates[0]:villager_idle_coordinates[2]])
+
+    population_text, population_threshold = extract_text(game_screen[population_coordinates[1]:population_coordinates[3],
+                                                                            population_coordinates[0]:population_coordinates[2]])
 
     game_time_text, game_time_threshold = extract_text(game_screen[game_time_coordinates[1]:game_time_coordinates[3],
                                                                         game_time_coordinates[0]:game_time_coordinates[2]])
+    
+
 
     # Print the extracted information
     print("Villager Queue:", villager_queue_text)
+    print("Villager number:", villager_number_text)
+    print("Villager idle:", villager_idle_text)
+    print("Population :", population_text)
     print("Game Time:", game_time_text)
 
     # Display the screen with rectangles
@@ -72,6 +98,9 @@ while True:
     # Display the thresholded images of detected text
     cv2.imshow('Villager Queue Text', villager_queue_threshold)
     cv2.imshow('Game Time Text', game_time_threshold)
+    cv2.imshow('villager number',villager_number_threshold)
+    cv2.imshow('villager idle',villager_idle_threshold)
+    cv2.imshow('population',population_threshold)
 
     # Wait for a short period before capturing the next frame
     if cv2.waitKey(5000) & 0xFF == ord('q'):
